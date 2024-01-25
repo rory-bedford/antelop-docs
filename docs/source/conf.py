f6ef09b8bf0b34329bdc6432df087ade42893b70
Rory Bedford <rorybedford@protonmail.com>
2024-01-25 17:23:47 +0000

    name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Antelope'
+copyright = '2023, Rory Bedford'
+author = 'Rory Bedford'
 
 release = '0.1'
 version = '0.1.0'
